a49aa16ade2cbb897c9bba3204471adbe69ca473
PetarVujetic <vujeticp@gmail.com>
2021-03-19 16:29:16 +0100

    Morgan - API Logging

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ const dotenv = require('dotenv')
 const db = require('./db')
 const Dream = require('./Dream/Dream')
 const dreamController = require('./Dream/dreamController')
+const morgan = require('morgan')
 
 // Config and variable setup
 dotenv.config({path: '.env'})
 const app = express()
 let PORT = process.env.PORT || 8000
+
+//Middlewares
+app.use(morgan('combined'))
 
 //Routes
 app.use('/api/dream', dreamController)
