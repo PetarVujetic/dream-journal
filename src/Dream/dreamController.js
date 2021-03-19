a275220765a0833515c73c616bd2eb75b90f985a
PetarVujetic <vujeticp@gmail.com>
2021-03-19 16:08:56 +0100

    Dream search with pagination

--- src/Dream/dreamController.js
+++ src/Dream/dreamController.js
@@ -15,18 +15,11 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-//Routes
+//ROUTES
 
 // Gets all dream types
 router.get('/dream-types', (req, res)=>{
   res.send(Dream.schema.path('dreamType').enumValues)
-})
-
-//Gets a single dream by id
-router.get('/:dreamId', async (req, res)=>{
-  const dream = await Dream.findById(req.params.dreamId)
-  if(!dream) return res.status(400).send('Dream not found')
-  res.status(200).send(dream)
 })
 
 // Creates a dream
@@ -43,17 +36,53 @@ router.post('/create-dream', async (req, res)=>{
   })
   try {
     dream.save()
+    res.status(201).send(dream)
   } catch (err) {
     console.log("Something went wrong with dream creation");
     res.status(500).send(err)
   }
-res.send(dream)
+
+})
+
+//Search by type, title and date
+// Pagination query params(page, limit)
+router.get('/search', async (req, res)=>{
+
+  let { dreamType, title, startDate, endDate} = req.body
+  const { page = 1, limit = 10} = req.query
+
+  let query = {
+    ...dreamType ? {"dreamType": dreamType} : {},
+    ...title ? { "title": title } : {},
+    ...startDate && endDate ? { createdDate: {$gt: startDate, $lt: endDate}} : {}
+  }
+  
+  try {
+  let dreams = await Dream.find(query).limit(limit * 1).skip((page - 1) * limit).sort('-createdDate')
+  if(!dreams) return res.status(400).send("No dreams found")
+  res.send({total: dreams.length, dreams});
+  } catch (err) {
+    return res.status(500).send(err)
+  }
+  
+})
+
+//Gets a single dream by id
+router.get('/:dreamId', async (req, res)=>{
+  try {
+    const dream = await Dream.findById(req.params.dreamId)
+    if(!dream) return res.status(400).send('Dream not found')
+    res.status(200).send(dream)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+ 
 })
 
 // Delete a dream
 router.delete('/:dreamId', async function (req, res) {
   await Dream.findByIdAndRemove(req.params.dreamId, function (err, dream) {
-    if (err) res.status(500).send("There was a problem deleting the dream.");
+    if (err) return res.status(500).send("There was a problem deleting the dream.");
     if (!dream) return res.send("Error finding the dream")
 
     res.status(200).send("Dream: " + dream.title + " was deleted.");
@@ -64,9 +93,11 @@ router.delete('/:dreamId', async function (req, res) {
 router.put('/:dreamId', async function (req, res) {
   await Dream.findByIdAndUpdate(req.params.dreamId, req.body, { new: true }, function (err, dream) {
     if (err) return res.status(500).send("There was a problem updating the dream.");
-    if (!dream) res.send("Error finding the dream")
-    res.status(200).send(dream);
+    if (!dream) return res.send("Error finding the dream to update")
+    res.status(201).send(dream);
   });
 });
+
+
 
 module.exports = router
